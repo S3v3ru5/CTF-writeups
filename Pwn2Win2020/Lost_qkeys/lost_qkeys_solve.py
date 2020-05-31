20d9982d933c8d345175605cfbed532de6dd56de
Vara Prasad <vara10110@gmail.com>
2020-05-31 23:19:26 +0530

    Create lost_qkeys_solve.py

---
+++ Pwn2Win2020/Lost_qkeys/lost_qkeys_solve.py
@@ -0,0 +1,33 @@
+from pwn import *
+from Crypto.Util.number import long_to_bytes
+
+counter = {}
+def init_prob(counter):
+	for i in range(520):
+		counter[i] = {0:0, 1:1}
+
+init_prob(counter)
+
+for i in range(100):
+	conn = remote("quantum.pwn2.win", 1337)
+	conn.recvline()
+	conn.sendline(b"0"*65)
+	# conn.interactive()
+	res = conn.recvline().strip()
+	print("i = ", i)
+	print("res = ", res)
+	val = list(map(int, bin(int(res, 16))[2:].zfill(520)[::-1]))
+	for i in range(520):
+		counter[i][val[i]] += 1
+	print("------------------*****************----------------")
+	conn.close()
+
+flag = ""
+
+for i in range(520):
+	if probabilities[i][0] < probabilities[i][1]:
+		flag += "1"
+	else:
+		flag += "0"
+
+print("flag = ", long_to_bytes(int(flag, 2)))
